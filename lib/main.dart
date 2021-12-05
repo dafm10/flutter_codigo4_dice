fc9c42c18e20d83d98b6f1287ac2ea8e06c7e839
David Fernandez Morales <davidfernandezmorales@MacBook-Pro-de-David.local>
2021-12-05 15:01:16 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  int numberDiceLeft = 1;
+  int numberDiceRight = 1;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurpleAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent,
+          title: const Text("DiceApp"),
+        ),
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  child: Image.asset(
+                    "assets/images/dice$numberDiceLeft.png",
+                  ),
+                  onPressed: () {
+                    numberDiceLeft = Random().nextInt(6) + 1;
+                    numberDiceRight = Random().nextInt(6) + 1;
+                    setState((){});
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Image.asset(
+                    "assets/images/dice$numberDiceRight.png",
+                  ),
+                  onPressed: () {
+                    numberDiceRight = Random().nextInt(6) + 1;
+                    numberDiceLeft = Random().nextInt(6) + 1;
+                    setState((){});
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
